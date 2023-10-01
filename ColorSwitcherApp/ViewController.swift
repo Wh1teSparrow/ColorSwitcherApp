ace82c2c1330a311ffbb80d66478c379a20278fb
Wh1teSparrow <wwhitesparroww@gmail.com>
2023-10-01 20:34:05 +0500

    Initial Commit

---
+++ ColorSwitcherApp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ColorSwitcherApp
+//
+//  Created by Kirill Syrtsev on 30.09.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colorView.layer.cornerRadius = colorView.frame.height / 2
+        colorView.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+    }
+    @IBAction func setupColorSliders() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+}
+

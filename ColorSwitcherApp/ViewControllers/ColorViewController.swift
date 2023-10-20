4e1f47e40d9757f0d227cea4d08e1c27e8b34e91
Wh1teSparrow <wwhitesparroww@gmail.com>
2023-10-21 00:00:58 +0500

    .

---
+++ ColorSwitcherApp/ViewControllers/ColorViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ColorViewController.swift
+//  ColorSwitcherApp
+//
+//  Created by Kirill Syrtsev on 20.10.2023.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
